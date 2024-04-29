cb0d67a00332919c3244f543c28773380944f9b8
AsyaAndreeva <asiara032@gmail.com>
2024-04-29 17:53:49 +0300

    Initial setup of Android app with project structure and Python integration

---
+++ app/src/main/java/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='Signify',
+    version='',
+    packages=[''],
+    package_dir={'': 'java'},
+    url='',
+    license='',
+    author='Ася Андреева',
+    author_email='',
+    description=''
+)
